8f2ab2869f84b2c63e67e33061beb66e819229d4
andrii <andrii.uvarov@nav.no>
2018-08-15 07:35:59 +0200

    save work

--- src/Collection.js
+++ src/Collection.js
@@ -6,6 +6,9 @@ class Collection extends Component {
     return (
       <div className="Collection">
         <h3><span className='h3'>{this.props.title}</span></h3>
+        <div className='CollectionContainer'>
+          {this.props.imgs.map(i => (<div style={{'background-image': `url(${i})`}} className='Image' />))}
+        </div>
         {this.props.tags.map(t => (<span key={t} className='tag'>{t}</span>))}
       </div>
     );
