b624b07809ddf16e2a18771795e6b8061a002f6b
Esben <esben@aomame.local>
2018-10-12 18:50:04 +0200

    LA

--- src/data/images/images.js
+++ src/data/images/images.js
@@ -17,7 +17,21 @@ import img25 from './sicily/DSC05850.jpg'
 import img26 from './sicily/DSC05886.jpg'
 import img27 from './sicily/DSC05934.jpg'
 
+// import img31 from './losangeles/DSC08932.jpg'
+// import img32 from './losangeles/DSC08942.jpg'
+import img33 from './losangeles/DSC08957.jpg'
+import img34 from './losangeles/DSC08972.jpg'
+import img35 from './losangeles/DSC08974.jpg'
+import img36 from './losangeles/DSC09011.jpg'
+import img37 from './losangeles/DSC09058.jpg'
+import img38 from './losangeles/DSC09060.jpg'
+import img39 from './losangeles/DSC09075.jpg'
+import img40 from './losangeles/DSC09083.jpg'
+import img41 from './losangeles/DSC09095.jpg'
+// import img42 from './losangeles/DSC09187.jpg'
+
 const images = [
+  {title: "Las Vegas/Los Angeles", images: [img33, img34, img35, img36, img37, img38, img39, img40, img41], tags: ["USA", "Los Angeles", "Las Vegas"]},
   {title: "Fuerteventura", images: [img, img1, img2, img3, img4], tags: ["Spain", "Fuerteventura"]},
   {title: "Rotterdam", images: [img11, img12, img13, img14], tags: ["Netherlands", "Rotterdam"]},
   {title: "Sicily", images: [img21, img22, img23, img24, img25, img26, img27], tags: ["Italy", "Sicily"]},
